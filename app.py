058b4f070a45b4d3770fd98023928738c876e740
watcharapon weeraborirak <s5919410022@sau.ac.th>
2021-03-01 16:56:07 +0700

    e

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from routers import line
 from dependencies import get_token_header
+import uvicorn
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
@@ -20,3 +21,7 @@ app.include_router(
 @app.get('/')
 def index(request: Request):
     return templates.TemplateResponse('index.html', context={'request': request})
+
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', port=8080, debug=True)
